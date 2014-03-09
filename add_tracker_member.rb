5d0c541c7f1d476caaa4c08d5b2226784289f1cb
jleo3 <joseph.leo3@gmail.com>
2014-03-09 10:16:46 -0400

    Add user to github repo

--- add_tracker_member.rb
+++ add_tracker_member.rb
@@ -3,20 +3,36 @@
 require "pry"
 require "dotenv"
 require "pivotal-tracker"
+require "octokit"
 require "json"
 require "yaml"
 
 Dotenv.load
-user = YAML.load_file("user.yaml")["user"]
 
-PivotalTracker::Client.token = ENV["PIVOTAL_TOKEN"]
-PivotalTracker::Client.use_ssl = ENV["PIVOTAL_PROJECT_USE_SSL"]
+def add_user_to_github
+  puts "adding user to github team"
+  github_client = Octokit::Client.new(
+    :login => ENV["GITHUB_LOGIN"], 
+    :password => ENV["GITHUB_PASSWORD"])
+  if github_client.add_team_member(
+    ENV["GITHUB_TEAM_ID"], 
+    @user["github_login"])
+    puts "success!"
+  end
+end
 
-pivotal_project = PivotalTracker::Project.find(ENV["PIVOTAL_PROJECT_ID"])
+def add_user_to_tracker
+  PivotalTracker::Client.token = ENV["PIVOTAL_TOKEN"]
+  PivotalTracker::Client.use_ssl = ENV["PIVOTAL_PROJECT_USE_SSL"]
 
+  pivotal_project = PivotalTracker::Project.find(ENV["PIVOTAL_PROJECT_ID"])
+  pivotal_project.memberships.add!(
+    role:     @user["role"], 
+    name:     @user["name"],  
+    email:    @user["email"],
+    initials: @user["initials"])
+end
 
-pivotal_project.memberships.add!(
-  role:     user["role"], 
-  name:     user["name"],  
-  email:    user["email"],
-  initials: user["initials"])
+@user = YAML.load_file("user.yaml")["user"]
+add_user_to_tracker
+add_user_to_github
